ec8268e4c1ddb280cf17bdb892b8f6802e3fb3a9
barbisliboni <niinaliboni@gmail.com>
2021-02-22 19:39:22 -0300

    Validação e configuração das páginas de login e cadastro

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  //TRAZENDO O OBJETO USERLOGIN
+  user: User = new User
+  confirmarSenha: string
+  tipoDeUser: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService, //INJEÇÃO DE DEPENDENCIAS SEMPRE FICAM DENTRO DO CONSTRUTOR
+    private router: Router
+  ) { }
+
+  ngOnInit(){ //apagar o void
+    window.scroll(0,0) //definindo que, a partir do momento que eu entrar na pagina,
+                      //começarei vendo os pixels das coordenadas x e y na posição 0
   }
 
+  //MÉTODO PARA CONFIRMAR SENHA
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+  }
+
+  //MÉTODO PARA OS TIPOS DE USUÁRIO
+  tipoUser(event: any){
+  this.tipoDeUser = event.target.value
+  }
+
+  //MÉTODO PARA CADASTRAR
+  cadastrar(){
+    this.user.tipoUsuario = this.tipoDeUser
+
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas!')
+    }else{
+      this.authService.cadastrar(this.user).subscribe((resp: User) =>{ //TRANSFORMANDO UM OBJETO TS EM FORMATO JSON
+       this.user = resp
+
+        //MANDANDO O USUÁRIO PARA UMA ROTA NO TYPESCRIPT
+        //PRIMEIRAMENTE, DEVO INJETAR UMA DEPENDENCIA NO CONSTRUCTOR CHAMADA ROUTER
+        this.router.navigate(['/login'])
+
+        alert('Usuário cadastrado com sucesso!')
+      }) 
+    }
+  }
 }
