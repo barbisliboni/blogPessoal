c1d81802d7ef6c3a00e8d479cf84ab12d1d7196c
barbisliboni <niinaliboni@gmail.com>
2021-02-23 19:48:55 -0300

    Criação do TemaComponent sem consumo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: 'login', component:  LoginComponent},
   {path: 'signup', component: SignupComponent},
 
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
