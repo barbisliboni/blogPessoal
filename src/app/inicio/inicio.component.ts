88d1584db18f04239f683c821c00406bde9a5195
barbisliboni <niinaliboni@gmail.com>
2021-03-01 22:05:21 -0300

    Implementando o PUT e o DELETE na Postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -9,74 +9,74 @@ import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+	selector: 'app-inicio',
+	templateUrl: './inicio.component.html',
+	styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
 
-  postagem: Postagem = new Postagem()
-  listaPostagens: Postagem[]
-  tema: Tema = new Tema()
-  listaTemas: Tema[]
-  idTema: number
-  user: User = new User()
-  idUser = environment.id
+	postagem: Postagem = new Postagem()
+	listaPostagens: Postagem[]
+	tema: Tema = new Tema()
+	listaTemas: Tema[]
+	idTema: number
+	user: User = new User()
+	idUser = environment.id
 
-  constructor(
-    private router: Router,
-    private postagemService: PostagemService,
-    private temaService: TemaService,
-    private authService: AuthService
-  ) { }
+	constructor(
+		private router: Router,
+		private postagemService: PostagemService,
+		private temaService: TemaService,
+		private authService: AuthService
+	) { }
 
-  ngOnInit() {
+	ngOnInit() {
 
-    if (environment.token == '') {
-      this.router.navigate(['/login'])
-    }
+		if (environment.token == '') {
+			this.router.navigate(['/login'])
+		}
 
-    this.getAllTemas()
-    this.getAllPostagens()
-  }
+		this.getAllTemas()
+		this.getAllPostagens()
+	}
 
-  getAllTemas() {
-    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
-      this.listaTemas = resp
-    })
-  }
+	getAllTemas() {
+		this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+			this.listaTemas = resp
+		})
+	}
 
-  findByIdTema() {
-    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
-      this.tema = resp
-    })
-  }
+	findByIdTema() {
+		this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+			this.tema = resp
+		})
+	}
 
-  getAllPostagens(){
-    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) =>{
-      this.listaPostagens = resp
-    })
-  }
+	getAllPostagens() {
+		this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+			this.listaPostagens = resp
+		})
+	}
 
-  findByIdUser(){
-    this.authService.getByIdUser(this.idUser).subscribe((resp: User) =>{
-      this.user = resp
-    })
-  }
+	findByIdUser() {
+		this.authService.getByIdUser(this.idUser).subscribe((resp: User) => {
+			this.user = resp
+		})
+	}
 
-  publicar() {
-    this.tema.id = this.idTema
-    this.postagem.tema = this.tema
+	publicar() {
+		this.tema.id = this.idTema
+		this.postagem.tema = this.tema
 
-    this.user.id = this.idUser
-    this.postagem.usuario = this.user
+		this.user.id = this.idUser
+		this.postagem.usuario = this.user
 
-    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
-      this.postagem = resp
+		this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+			this.postagem = resp
 
-      alert('Post was succesfully made!')
-      this.postagem = new Postagem() //limpando os campos 
-      this.getAllPostagens()
-    })
-  }
+			alert('Post was succesfully made!')
+			this.postagem = new Postagem() //limpando os campos 
+			this.getAllPostagens()
+		})
+	}
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
